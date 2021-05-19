478359faad560acba76a6dfbdbcbf49e1385f3bf
Alejandro Curiel <alex_curielg@hotmail.com>
2021-05-18 21:15:30 -0500

    Setting up the base project with gulp and sass

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const { src, dest, watch} = require('gulp');
+const sass = require('gulp-sass');
+
+sass.compiler = require('dart-sass');
+
+function compilarSASS(){
+    return src("./src/scss/app.scss")
+    .pipe(sass())
+    .pipe(dest("./build/css"));
+}
+
+function minificarCss(){
+    return src("./src/scss/app.scss")
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe(dest("./build/css"))
+}
+function expandirCss(){
+    return src("./src/scss/app.scss")
+    .pipe(sass({
+        outputStyle: 'expanded'
+    }))
+    .pipe(dest("./build/css"))
+}
+
+function watchArchivos(){
+    watch("./src/scss/**/*.scss", compilarSASS); // * = La carpeta actual - ** = Todos los arcivos con esa extension
+
+}
+exports.compilarSASS = compilarSASS
+exports.minificarCss = minificarCss
+exports.expandirCss = expandirCss
+exports.watchArchivos = watchArchivos
